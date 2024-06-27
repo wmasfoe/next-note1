7a40a32f40f9bb711edd01233777f2849f9ad8fa
lijiaqi <lijq1103@gmail.com>
2024-06-27 19:24:58 +0800

    feat: 调用api上传文件

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,15 +1,54 @@
+'use client'
+import { useTransition } from 'react'
+import { useRouter } from 'next/navigation'
 
-export default async function Upload() {
+export default function Upload() {
+
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter()
+
+  async function handleInputChange(e) {
+    const fileInput = e.target
+
+    if (!fileInput.files || fileInput.files.length === 0) {
+      console.warn("files list is empty");
+      return;
+    }
+
+    const file = fileInput.files[0];
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await fetch("/api/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        console.error("something went wrong");
+        return;
+      }
+
+      const data = await response.json();
+      startTransition(() => router.push(`/note/${data.uid}`));
+      // 刷新客户端缓存
+      startTransition(() => router.refresh());
+
+    } catch (error) {
+      console.error("something went wrong");
+    }
+
+    // 重置 file input
+    e.target.type = "text";
+    e.target.type = "file";
+  }
 
   return <>
-    <form method="post" enctype="multipart/form-data">
-      <div>
-        <label for="file">Import .md File</label>
-        <input type="file" id="file" name="file" multiple style={{ position : "absolute", clip: "rect(0 0 0 0)" }} />
-      </div>
-      <div>
-        <button>submit</button>
-      </div>
-    </form>
+    <div>
+      <label for="file">Import .md File</label>
+      <input type="file" id="file" name="file" multiple hidden onChange={handleInputChange} />
+    </div>
   </>
 }
